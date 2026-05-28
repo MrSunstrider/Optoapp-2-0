75530a47a598a7c91629ad017d92df289c751818
Mr.Sunstrider <jaermadera@gmail.com>
2026-05-27 20:07:53 -0500

    chore(release): bump to v1.1.4

--- optoapp/build.gradle.kts
+++ optoapp/build.gradle.kts
@@ -30,8 +30,8 @@ android {
         applicationId = "com.example.optoapp"
         minSdk = 24
         targetSdk = 36
-        versionCode = 5
-        versionName = "1.1.3"
+        versionCode = 6
+        versionName = "1.1.4"
         
         multiDexEnabled = true
 
