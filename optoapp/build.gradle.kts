4bf507724d547981058fce558cb250e5525d46af
Mr.Sunstrider <jaermadera@gmail.com>
2026-06-18 08:37:19 -0500

    chore(release): bump version to 1.6.1 (versionCode 24)

--- optoapp/build.gradle.kts
+++ optoapp/build.gradle.kts
@@ -30,8 +30,8 @@ android {
         applicationId = "com.example.optoapp"
         minSdk = 24
         targetSdk = 36
-        versionCode = 23
-        versionName = "1.6.0"
+        versionCode = 24
+        versionName = "1.6.1"
         
         multiDexEnabled = true
 
