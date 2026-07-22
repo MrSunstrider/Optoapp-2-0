e8f424416138e7a3f5c5a454e3b0f627b4de9552
Mr.Sunstrider <jaermadera@gmail.com>
2026-07-22 01:05:59 -0500

    chore: bump version to 1.15.4 (versionCode 44)

--- optoapp/build.gradle.kts
+++ optoapp/build.gradle.kts
@@ -66,8 +66,8 @@ android {
         applicationId = "com.example.optoapp"
         minSdk = 24
         targetSdk = 36
-        versionCode = 43
-        versionName = "1.15.3"
+        versionCode = 44
+        versionName = "1.15.4"
 
         multiDexEnabled = true
 
