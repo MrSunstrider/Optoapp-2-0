0aac5db0c1b4c7fe4cbca7f1dfd44a7aa801c48c
Mr.Sunstrider <jaermadera@gmail.com>
2026-07-21 20:09:58 -0500

    chore: bump version to 1.15.1 (versionCode 41)

--- optoapp/build.gradle.kts
+++ optoapp/build.gradle.kts
@@ -66,8 +66,8 @@ android {
         applicationId = "com.example.optoapp"
         minSdk = 24
         targetSdk = 36
-        versionCode = 40
-        versionName = "1.15.0"
+        versionCode = 41
+        versionName = "1.15.1"
 
         multiDexEnabled = true
 
